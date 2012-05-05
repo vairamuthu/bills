2f2430d27a2f932d245495f34a63aafc40a45ae2
unknown <vairamuthu@vairamuthu-PC.(none)>
2012-05-05 08:53:42 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bills::Application.config.secret_token = 'b5eca4005045c7d142084014e59af08de92eed35666aa7bdef0b097d305858d14d3357383b6001ae74eea9bd4e95b40ebbfa890accdb50e3f3948ba50007451a'
